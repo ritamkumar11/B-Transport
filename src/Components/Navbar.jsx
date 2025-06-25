4c366e0ac842ee120f170db6e401ad879dca5818
ritamkumar11 <ritamkumar111005@gmail.com>
2025-06-25 18:11:33 +0530

    Initial Commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+const Navbar = () => {
+    return (
+        <>
+            <div className="Container">
+                <div className="NavBar">
+                    <div className="Scontainer1">
+                        <div className="logo"> </div>
+                        <a href="#">B-Transport</a>
+                    </div>
+
+                    <div className="Scontainer2">
+                        <a href="#">Services</a>
+                        <a href="#">About</a>
+                        <a href="#">Contact</a>
+                        <a href="#">Login/SignUp</a>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Navbar
